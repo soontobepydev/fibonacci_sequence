c26a7df93ec3cb2f9293040c6d64ad266228d6cf
soontobepydev <andre.97asd@hotmail.com>
2022-02-19 23:39:19 +0000

    - added function that gets any positive number within the fibonacci sequence - added error checking for input - added greeting method that grabs random greet from a file with greetings

---
+++ src/fibonacci_sequence.py
@@ -0,0 +1,41 @@
+import random
+import math
+
+def get_greetings_from_file():
+    file_path = 'src\greetings.txt'
+    with open(file_path) as file:
+        greetings = file.readlines()
+    return greetings
+    
+def greet():
+    greetings = get_greetings_from_file()
+    print(random.choice(greetings))
+
+def get_fibonacci(n):
+    Phi = (1 + math.sqrt(5))/2
+    phi = (1 - math.sqrt(5))/2
+
+    return n if n <= 2 else round( ((Phi  ** n) - (phi ** n ) )/ math.sqrt(5) ) 
+
+def main():
+    greet()
+    n = 0
+
+    while (n != -1):
+        print("what's the n number in the fibonacci sequence you want to see")
+        print('type a number between 0 and 1. Insert -1 to end')
+        print()
+        try:
+            n =int(input('N = '))
+            if(n == -1):
+                continue
+        except:
+            print()
+            print('you must provide an integer number from 0 and above')
+            print()
+        else:
+            print(f'F[{n}]={get_fibonacci(n)}')
+
+        
+if __name__ == '__main__':
+    main()
